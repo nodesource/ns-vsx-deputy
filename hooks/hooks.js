9703ee4d96e23c574fe9dac018cb9c19534e2ea6
Thorsten Lorenz <thlorenz@gmx.de>
2018-10-31 11:42:45 -0500

    hooks: enabling long stacktraces automatically

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -1,4 +1,9 @@
 'use strict'
 
+// setup long + async stack traces
+const v8 = require('v8')
 require('trace')
+v8.setFlagsFromString('--stack_trace_limit=100')
+
+// setup promise hooks
 require('./deputy-promise-guard')
