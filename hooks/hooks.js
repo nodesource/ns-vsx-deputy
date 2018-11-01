78baa12ea3cc31456cf809174f9fa803dfdf3e96
Thorsten Lorenz <thlorenz@gmx.de>
2018-11-01 11:38:31 -0500

    hooks: enabling long stack traces not working - removed

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -1,9 +1,7 @@
 'use strict'
 
 // setup long + async stack traces
-const v8 = require('v8')
 require('trace')
-v8.setFlagsFromString('--stack_trace_limit=100')
 
 // setup promise hooks
 require('./deputy-promise-guard.multi')
