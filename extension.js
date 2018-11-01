50feed503de006aac2674710983a4377d75cf915
Thorsten Lorenz <thlorenz@gmx.de>
2018-11-01 18:16:25 -0500

    task: initial config and main snippet
    - optaining hooks path via function

--- extension.js
+++ extension.js
@@ -1,30 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+'use strict'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const { commands } = require('vscode')
+const path = require('path')
+
 function activate(context) {
+  console.log('"ns-vsx-deputy" is now active!')
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "ns-vsx-deputy" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+  const disposable = commands
+    .registerCommand('deputy.hooksPath', function() {
+      return path.join(__dirname, 'hooks', 'hooks.js')
+  })
+  context.subscriptions.push(disposable)
 }
-exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
+function deactivate() {}
+
+module.exports = { activate, deactivate }
